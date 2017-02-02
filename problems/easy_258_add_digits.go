88fbeee8db324deee8126e99e4f8eeafc81c1c74
Kettan <iamken1204@gmail.com>
2017-02-02 10:42:58 +0800

    Update solution for 258

--- problems/easy_258_add_digits.go
+++ problems/easy_258_add_digits.go
@@ -1,33 +1,10 @@
 package problems
 
-import (
-	"log"
-	"strconv"
-	"strings"
-)
-
 // AddDigits represents
 // Easy 258. Add Digits
 // https://leetcode.com/problems/add-digits/
+// 這個問題有公式... dr(n) = 1 + (n - 1) % 9
+// https://en.wikipedia.org/wiki/Digital_root
 func AddDigits(num int) int {
-	var res int
-	var err error
-	numS := strconv.Itoa(num)
-	numSA := strings.Split(numS, "")
-	if len(numSA) < 2 {
-		res, err = strconv.Atoi(numSA[0])
-		if err != nil {
-			log.Fatalln(err)
-		}
-		return res
-	}
-	var next int
-	for _, val := range numSA {
-		tmp, err := strconv.Atoi(val)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		next += tmp
-	}
-	return AddDigits(next)
+	return 1 + (num-1)%9
 }
