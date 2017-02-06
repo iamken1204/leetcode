2fcc6bca85e519abc355abccc6b6aac3c31e2687
Kettan <iamken1204@gmail.com>
2017-02-06 17:15:17 +0800

    Improved version of easy_167

--- problems/easy_167_two_sum_II_input_array_is_sorted.go
+++ problems/easy_167_two_sum_II_input_array_is_sorted.go
@@ -4,22 +4,14 @@ package problems
 // 167. Two Sum II - Input array is sorted
 // https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 func TwoSum2(numbers []int, target int) []int {
-	var res []int
-	var i int
-	var done bool
-	maxI := len(numbers) - 2
-	for i <= maxI && numbers[i] <= target {
-		for j := i + 1; j < len(numbers); j++ {
-			if numbers[i]+numbers[j] == target {
-				res = []int{i + 1, j + 1}
-				done = true
-				break
-			}
+	left := 0
+	right := len(numbers) - 1
+	for numbers[left]+numbers[right] != target {
+		if numbers[left]+numbers[right] > target {
+			right--
+		} else {
+			left++
 		}
-		if done {
-			break
-		}
-		i++
 	}
-	return res
+	return []int{left + 1, right + 1}
 }
